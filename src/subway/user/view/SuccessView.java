1ffadc6f95af60b425d9fd7a24fc7c7a189f8888
aseasf <milandy6@gmail.com>
2018-10-09 20:42:54 +0900

    SucccessView 충돌 수정

--- src/subway/user/view/SuccessView.java
+++ src/subway/user/view/SuccessView.java
@@ -1,11 +1,7 @@
 package subway.user.view;
 
 public class SuccessView {
-<<<<<<< HEAD
 	public static void successMessage(String message){
-=======
-	public static void successMessage(String message) {
->>>>>>> 411382a5dab6e6d265612c9f46c2ea57f396b063
 		System.out.println(message);
 	}
 }
