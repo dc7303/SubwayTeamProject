59736b669c71947ad4ac4249750a63f3144bc68e
aseasf <milandy6@gmail.com>
2018-10-11 17:26:16 +0900

    SuccessView에 vListView로 orderSelectVector()내용 추가

--- src/subway/user/view/SuccessView.java
+++ src/subway/user/view/SuccessView.java
@@ -2,6 +2,7 @@ package subway.user.view;
 
 
 import java.util.List;
+import java.util.Vector;
 
 import subway.user.model.dto.OrderDTO;
 
@@ -17,5 +18,11 @@ public class SuccessView {
 			System.out.println(list.get(i));
 		}
 	}
- 
+	
+	
+	public static void vListView(List<Vector<Object>> vList) {
+		for(int i = 0; i < vList.size(); i++) {
+			System.out.println(vList.get(i));
+		}
+	}
 }
