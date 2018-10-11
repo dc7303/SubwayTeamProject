53c2ab5cb3bf60c11087dd0a5b2f3529c4aecd4f
aseasf <milandy6@gmail.com>
2018-10-11 17:23:39 +0900

    DAO, DAOImpl 에 orderSelectVector() 추가

--- src/subway/user/model/dao/OrderDAO.java
+++ src/subway/user/model/dao/OrderDAO.java
@@ -2,6 +2,7 @@ package subway.user.model.dao;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Vector;
 
 import subway.user.model.dto.OrderDTO;
 
@@ -17,5 +18,8 @@ public interface OrderDAO {
 	
 	public List<OrderDTO> orderSelect(String userId) throws SQLException;
 	
+	public List<Vector<Object>> orderSelectVector(String userId, boolean isMyMenu) throws SQLException;
+	
 	public List<OrderDTO> myMenuSelect(String userID) throws SQLException;
 }
+
